8bb2e5c1d0150501e7e7770c365efcd77790d805
nittamatama <blackmnshin@gmail.com>
2021-05-15 21:11:58 +0900

    仮のビューを設置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,13 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :image)
   end
 
 end
