b339b83ceb1541e3605451a89281bd36689f6535
nittamatama <blackmnshin@gmail.com>
2021-05-16 19:39:57 +0900

    ルーティングを入れ子構造に修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  resources :posts, only: [:index, :new]
+  resources :posts, only: [:index, :new] do
+    resources :likes, only: [:create]
+  end
 end
