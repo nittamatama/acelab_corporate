df9b4a38891399a3c69a0e909aa50c5e33102a2d
nittamatama <blackmnshin@gmail.com>
2021-05-17 09:08:29 +0900

    like機能のコントローラーまで作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
     @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  resources :posts, only: [:index, :new] do
+  resources :posts, only: [:index, :new, :create, :show] do
     resources :likes, only: [:create]
   end
 end
