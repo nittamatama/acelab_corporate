0908dab25dbc7fe249ddcfe54130f61d9ce4ed64
nittamatama <blackmnshin@gmail.com>
2021-05-19 20:21:27 +0900

    詳細ページに編集と削除リンクを設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  resources :posts, only: [:index, :new, :create, :show, :edit] do
+  resources :posts do
     resources :likes, only: [:create, :destroy]
   end
 end
