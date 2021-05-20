fbedd64fcedf5a7a52d0dde4b1ec4ab6be0b9d51
nittamatama <blackmnshin@gmail.com>
2021-05-20 19:53:35 +0900

    モデルにdependent追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :likes, dependent: :destroy 
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
   validates :title, presence: true
