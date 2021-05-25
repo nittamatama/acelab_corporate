4b6f6bffb531d922bf86f151ad7709f1426aabb6
nittamatama <blackmnshin@gmail.com>
2021-05-25 09:35:51 +0900

    postsコントローラーにmove_to_indexアクションを設置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show]
   
   def index
     @posts = Post.all
@@ -54,6 +55,12 @@ class PostsController < ApplicationController
   
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def move_to_index
+    unless admin_signed_in?
+      redirect_to action: :index
+    end
   end
 end
 
