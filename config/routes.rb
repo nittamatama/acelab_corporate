24e4a67b888bbc024629eb53aae30749d412365d
nittamatama <blackmnshin@gmail.com>
2021-06-07 23:29:53 +0900

    staticpageコントローラーとcompanyアクション作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/company'
+  get 'static_pages/home'
+  get 'static_pages/help'
   devise_for :admins
   root to: 'posts#index'
   resources :posts do
