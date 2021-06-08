d45cf0ec335b03f25d5c948e63b9a904e4fc1c2a
nittamatama <blackmnshin@gmail.com>
2021-06-08 09:37:04 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/company'
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get 'static_pages/company', to: 'static_pages#company'
   devise_for :admins
   root to: 'posts#index'
   resources :posts do
