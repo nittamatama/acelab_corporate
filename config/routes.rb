17efbca470efe25fb977f8cae06dbddffdce855b
nittamatama <blackmnshin@gmail.com>
2021-06-13 14:19:17 +0900

    文字並び　行揃え修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/company', to: 'static_pages#company'
   get 'static_pages/employee', to: 'static_pages#employee'
+  get 'static_pages/task', to: 'static_pages#task'
+  
   devise_for :admins
   root to: 'posts#index'
   resources :posts do
