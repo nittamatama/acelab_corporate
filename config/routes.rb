29e339a47a042c003809da3419028e6cf371b8f8
nittamatama <blackmnshin@gmail.com>
2021-06-23 20:44:22 +0900

    コラム一覧ページのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   get 'static_pages/task', to: 'static_pages#task'
   get 'static_pages/faq', to: 'static_pages#faq'
   get 'static_pages/product_achivement', to: 'static_pages#product_achivement'
+  get 'posts/index'
   
   devise_for :admins
   root to: 'static_pages#index'
-  resources :posts do
+  resources :posts, only: :index do
     resources :likes, only: [:create, :destroy]
   end
 end
