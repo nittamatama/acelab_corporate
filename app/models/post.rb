23324b51533c6636dfdc1939f2864ff3f5b5c01c
nittamatama <blackmnshin@gmail.com>
2021-06-26 22:43:21 +0900

    ビュー修正と、adminとアソシエーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy 
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
+  belongs_to :admin
 
   validates :title, presence: true
   validates :text,  presence: true
