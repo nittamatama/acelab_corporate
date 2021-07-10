f9cafc616823d894a76a9201cce16a1fc3bb19d8
nittamatama <blackmnshin@gmail.com>
2021-07-10 13:18:46 +0900

    form_with実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
- 
+  before_action :set_post, only: [:edit, :show]
+  
   def index
     @posts = Post.search(params[:keyword]).order("created_at DESC")
   end
@@ -9,6 +10,16 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+    @comments = @post.comments
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_post
     @post = Post.find(params[:id])
   end
 
