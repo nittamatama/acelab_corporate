3fbcdb6aea3e0e99ba29cdbfc2e20ac5b9605e5c
nittamatama <blackmnshin@gmail.com>
2021-07-12 20:23:36 +0900

    application_controller編集　サインイン後の遷移先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    if current_admin
+      flash[:notice] = "ログインに成功しました"
+      root_path
+    else
+      flash[:notice] = "新規登録に成功しました"
+      root_path
+    end
+  end
+  
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
