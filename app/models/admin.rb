2c3cf8a9e96d105d46acb6b689fe939430749852
nittamatama <blackmnshin@gmail.com>
2021-07-14 19:29:24 +0900

    with_option外してみる

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -6,14 +6,12 @@ class Admin < ApplicationRecord
 
          has_many :posts
   
-         with_options presence: true do
-           validates :first_name, :last_name,
+           validates :first_name, :last_name, presence: true,
              format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/, message: "を入力してください"}
-           validates :password,
+           validates :password, presence: true,
              format: { with: /\A[a-z0-9]+\z/i, message: "は半角英数字のみ登録可能です" }
-           validates :email, uniqueness: true,
+           validates :email, uniqueness: true, presence: true,
              format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "は半角英数字のみ登録可能です" }
-         end
 
   def full_name
     self.first_name + " " + self.last_name
