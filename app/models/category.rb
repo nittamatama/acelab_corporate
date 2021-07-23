007913f668d4d7e6d4c9409f2bff2b4a75e4786a
nittamatama <blackmnshin@gmail.com>
2021-07-23 21:51:32 +0900

    Categoryモデルを作成し、Avtivehashを継承

---
+++ app/models/category.rb
@@ -0,0 +1,9 @@
+class Category < ActiveHash::Base
+ self.data = [
+   { id: 1, name: '--' },
+   { id: 2, name: 'カテゴリ1' },
+   { id: 3, name: 'カテゴリ2' },
+   { id: 4, name: 'カテゴリ3' },
+   { id: 5, name: 'カテゴリ4' }
+ ]
+ end
