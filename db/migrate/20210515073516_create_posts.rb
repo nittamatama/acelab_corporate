cfc432f14da6ea969f515db1282991a1620977af
nittamatama <blackmnshin@gmail.com>
2021-07-24 13:22:15 +0900

    dependent追加

--- db/migrate/20210515073516_create_posts.rb
+++ db/migrate/20210515073516_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.string :title, null: false
-      t.references :admin, foreign_key: true
+      t.references :admin, foreign_key: true, dependent: :destory
       t.timestamps
     end
   end
