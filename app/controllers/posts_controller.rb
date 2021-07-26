1fe21773863e901d759cd7bbe9ab874ba69f70f9
nittamatama <blackmnshin@gmail.com>
2021-07-26 09:43:11 +0900

    ストロングパラメータ設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content,:image)
+    params.require(:post).permit(:title, :content,:image, :genre_id)
   end
 
   def set_post
