38ffb23239a4afbaee6ec98ac98a64627df6f6c8
nittamatama <blackmnshin@gmail.com>
2021-07-26 09:32:03 +0900

    Acitivehashのアソシエーション記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :likes, dependent: :destroy 
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
   has_many :comments
   belongs_to :admin
+  belongs_to :category
 
   has_one_attached :image
   has_rich_text :content
